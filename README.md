cc1dd6653ccb75bf2263401ef28670eb3a9f6cfe
Abdul Hannan <abdunknownsamurai@gmail.com>
2024-12-04 19:29:56 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Digital-Clock---Project---HTML---CSS---JS---AHCreativeCode
+ A stylish Digital Clock created using HTML, CSS, and JavaScript. This project displays the current time in a 12-hour format with AM/PM indication, styled with a modern, aesthetic UI
---
+++ digitalclock.js
@@ -0,0 +1,18 @@
+// Digital - Clock - Project - HTML - CSS - JS - AHCreativeCode
+
+function clockTime(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours =  hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById('clocki').textContent = timeString;
+
+}
+
+clockTime();
+setInterval(clockTime, 1000)
